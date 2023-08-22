c093cf23b58ad69bb1693b730993c74d2879c768
OMT26 <89383978+OMT26@users.noreply.github.com>
2023-08-22 11:30:14 +0200

    Update install.py

--- install.py
+++ install.py
@@ -5,7 +5,4 @@ def install(package):
     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
 install('mysql-connector-python')
-install('python-dateutil')
-install('numpy')
-install('numba')
 install('python-dotenv')
